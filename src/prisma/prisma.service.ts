eab71abfed2a433ff389ad206bca2c99e468a3c0
Narek <narekraghatspanyan.uwork@gmail.com>
2023-05-30 14:38:31 +0400

    created auth guard, jwt strategy

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -13,6 +13,12 @@ export class PrismaService extends PrismaClient {
         },
       },
     });
-    console.log(config.get('DATABASE_URL'))
+    console.log(config.get('DATABASE_URL'));
+  }
+  cleanDb() {
+    return this.$transaction([
+      this.bookmark.deleteMany(),
+      this.user.deleteMany(),
+    ]);
   }
 }
